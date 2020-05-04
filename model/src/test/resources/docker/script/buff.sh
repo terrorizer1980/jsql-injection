aa40e5b9fbfa89d26ee012cafeb119d8c8761f35
ron190 <ron190@ymail.com>
2020-05-05 00:43:19 +0200

    Add docker host

--- model/src/test/resources/docker/script/buff.sh
+++ model/src/test/resources/docker/script/buff.sh
@@ -1,5 +1,5 @@
 # Buff MySQL
-cat <<EOF | sudo docker exec -i jsql-mysql /bin/bash
+cat <<EOF | docker -H ${DOCKER_HOST} exec -i jsql-mysql /bin/bash
     mysql -uroot -pmy-secret-pw -e ' 
         SET GLOBAL max_connections = 100000; 
         SET GLOBAL thread_cache_size = 16384; 
@@ -8,7 +8,7 @@ cat <<EOF | sudo docker exec -i jsql-mysql /bin/bash
 EOF
 
 # Check SQL Server connection
-sudo docker exec -i jsql-sqlserver /opt/mssql-tools/bin/sqlcmd \
+docker -H ${DOCKER_HOST} exec -i jsql-sqlserver /opt/mssql-tools/bin/sqlcmd \
 -S "tcp:127.0.0.1,1434" \
 -U SA \
 -P "yourStrong(!)Password" \
