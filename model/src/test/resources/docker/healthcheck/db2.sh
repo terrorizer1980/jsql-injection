12213ab4f2d65140aca18198645085899f6035f6
ron190 <ron190@ymail.com>
2020-05-19 21:42:17 +0200

    Fix .m2 and db2 healthcheck

--- model/src/test/resources/docker/healthcheck/db2.sh
+++ model/src/test/resources/docker/healthcheck/db2.sh
@@ -5,16 +5,16 @@ set -e
 retry=0
 
 until cat <<EOF | docker exec -i -w /database/config/db2inst1/sqllib/bin -u db2inst1 jsql-db2 bash
-    pwd
-    export DB2INST1_PASSWORD=test
-    export DB2INSTANCE=db2inst1
-    export DB2LIB=/database/config/db2inst1/sqllib/lib
-    export DB2_HOME=/database/config/db2inst1/sqllib
-    export DBNAME=testdb
-    export DBPORT=50000
-    ./db2 list active databases
-    ./db2 connect to testdb IN SHARE MODE user db2inst1 using test
-    ./db2 select 1 as jsqlColumn from sysibm.sysversions
+pwd
+export DB2INST1_PASSWORD=test
+export DB2INSTANCE=db2inst1
+export DB2LIB=/database/config/db2inst1/sqllib/lib
+export DB2_HOME=/database/config/db2inst1/sqllib
+export DBNAME=testdb
+export DBPORT=50000
+./db2 list active databases
+./db2 connect to testdb IN SHARE MODE user db2inst1 using test
+./db2 select 1 as jsqlColumn from sysibm.sysversions
 EOF
 do
   retry=$((retry+1))
